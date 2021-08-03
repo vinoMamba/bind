9cb190cd8b7dbc5fd833ae5f242a5e8883485047
vino <vino0908@outlook.com>
2021-08-03 21:39:39 +0800

    polyfill

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-function bind(asThis,...agrs) {
-    const fn = this
-    return function (...args2) {
-        return fn.call(asThis,...agrs,...args2)
+var slice = Array.prototype.slice
+function bind(asThis) {
+    var fn = this
+    var agrs =slice.call(arguments,1)
+    if(typeof fn !== 'function') {
+        throw new Error('必须是函数')
+    }
+    return function () {
+        var args2 = slice.call(arguments,0)
+        return fn.apply(asThis,agrs.concat(args2))
     };
 }
 
